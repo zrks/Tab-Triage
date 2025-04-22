61820ea876a5d5648e45ba9e9d797432358f2d4d
marcis.veveris <marcis.veveris@outlook.com>
2025-04-22 10:31:24 +0000

    add tab limiting

--- background.js
+++ background.js
@@ -1,8 +1,35 @@
 // For Chrome compatibility
 const browser = typeof browser === 'undefined' ? chrome : browser;
 
-(function () {
-  browser.runtime.onInstalled.addListener(() => {
+(async function () {
+  await browser.runtime.onInstalled.addListener(() => {
     console.log('Tab Triage Extension installed.');
   });
+
+  let maxTabsPerWindow = await browser.storage.local.get('maxTabsPerWindow').then((result) => {
+    if (result.maxTabsPerWindow === undefined) {
+      console.log("maxTabsPerWindow not found, setting to 10")
+      return 10
+    } else {
+      return result.maxTabsPerWindow;
+    }
+  });
+
+  browser.tabs.onCreated.addListener(async (tab) => {
+    console.log("Tab created");
+
+    if (tab.windowId === undefined) {
+      return;
+    }
+
+    const tabsInWindow = await browser.tabs.query({ windowId: tab.windowId });
+
+    if (tabsInWindow.length > maxTabsPerWindow) {
+      console.log(`Maximum tabs (${maxTabsPerWindow}) reached, removing new tab.`);
+      browser.tabs.remove(tab.id);
+    }
+  });
+
+
+
 })();
--- options.js
+++ options.js
@@ -1,15 +1,22 @@
 // Load saved options
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.storage.local.get(['closeDuplicateTabs', 'highlightUnreadTabs'], (result) => {
+    const loadSettings = () => {
+    chrome.storage.local.get(['closeDuplicateTabs', 'highlightUnreadTabs', 'maxTabsPerWindow'], (result) => {
       if (typeof result.closeDuplicateTabs !== 'undefined') {
         document.getElementById('closeDuplicateTabs').checked = result.closeDuplicateTabs;
       }
       if (typeof result.highlightUnreadTabs !== 'undefined') {
         document.getElementById('highlightUnreadTabs').checked = result.highlightUnreadTabs;
       }
+        if (typeof result.maxTabsPerWindow !== 'undefined') {
+            document.getElementById('maxTabsPerWindow').value = result.maxTabsPerWindow;
+          }else{
+            document.getElementById('maxTabsPerWindow').value = 10;
+          }
     });
+    };
+    loadSettings()
   });
-  
   // Save options
   document.getElementById('save').addEventListener('click', () => {
     const closeDuplicateTabs = document.getElementById('closeDuplicateTabs').checked;
@@ -17,7 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
   
     chrome.storage.local.set({
       "closeDuplicateTabs": closeDuplicateTabs,
-      "highlightUnreadTabs": highlightUnreadTabs
+      "highlightUnreadTabs": highlightUnreadTabs,
+      "maxTabsPerWindow": Number(document.getElementById('maxTabsPerWindow').value)
+
     }, () => {
       const status = document.getElementById('status');
       status.textContent = 'Settings saved!';
